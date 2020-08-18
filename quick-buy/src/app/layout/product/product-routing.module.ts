29834bd6746e8ce97fca031ec48c55893b8ff68f
= <=>
2020-08-18 10:00:46 +0530

    commit
    commit

--- quick-buy/src/app/layout/product/product-routing.module.ts
+++
@@ -1,8 +0,0 @@
-import { RouterModule, Routes } from '@angular/router';
-import { ProductComponent } from './product.component';
-
-const routes: Routes = [
-  { path: '', component: ProductComponent }
-];
-
-export const ProductRoutes = RouterModule.forChild(routes);
--- quick-buy/src/app/layout/users/user-routing.module.ts
+++
@@ -1,8 +0,0 @@
-import { RouterModule, Routes } from '@angular/router';
-import { UserComponent } from './user/user.component';
-
-const routes: Routes = [
-  { path: '', component: UserComponent }
-];
-
-export const UserRoutes = RouterModule.forChild(routes);
